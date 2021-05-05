af5c56f1068cfc3181e14f36e9a0247c1afb9f96
MohammadALAbbad <>
2021-05-05 11:34:54 +0000

    JS added

---
+++ JS.js
@@ -0,0 +1,10 @@
+var username = prompt('what is your name')
+if (username === 'mohd') {document.write('hello mohd')}
+else if (username === 'alex'){alert('hello alex')}
+else if (username === 'sarah') {console.log('hello sarah')}
+else {alert ('hello user')}
+
+var book = prompt('pick a book')
+if (book === 'book1') {document.write('<img "src="book1.jpg>"')}
+else if (book === 'book2'){document.write('<img src="book2.jpg"')}
+else {alert('not inserted in our database')}
