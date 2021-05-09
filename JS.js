0aebff20c4e13834b97685083b8a25e42add2f2e
MohammadALAbbad <>
2021-05-09 11:44:44 +0000

    JS functions added

--- JS.js
+++ JS.js
@@ -1,4 +1,4 @@
-/*var username = prompt('what is your name')
+var username = prompt('what is your name')
 if (username === 'mohd') {document.write('hello mohd')}
 else if (username === 'alex'){alert('hello alex')}
 else if (username === 'sarah') {console.log('hello sarah')}
@@ -13,16 +13,45 @@ alert ('choose book1 or book2')
 
 var userBookfav = prompt('what is your favorite book?' )
 
-while(userBookfav !=== 'book1' && userBookfav !=== 'book2'){
+while(userBookfav !== 'book1' && userBookfav !== 'book2'){
   userBookfav = prompt('try again')
 }
 var bookCount = prompt ('how many book do you want us to display?')
 var image = ''
 for(var i=0; i< bookCount; i++) {
-  if (userBookfav === 'book1'){
-    document.write('<img src="book1.jpg"')
+  if (userBookfav == 'book1'){
+    document.write('<img src="book1.jpg">') 
+    console.log(bookCount)
   }
-  else if(userBookfav === 'book2'){
-    image = image + '<img src="book2.jpg"'
+  else if(userBookfav == 'book2'){
+    document.write('<img src="book2.jpg">')
   }
 }
+
+
+var userFavBook = prompt('Whats your favourite book?')
+
+function userBook(){
+ while(userFavBook !== 'book1' && userFavBook !=='book2'){
+  userFavBook = prompt('Try again !')
+ }
+}
+userBook()
+
+var bookCount = prompt('How many book do you want to see ?')
+
+function gettingBooks(){
+
+var image = ''
+for(var x = 0; x < bookCount ;x++){
+  if(userFavBook ==='book1'){
+    image = image + '<img src="book1.jpg">'
+  }else if( userFavBook ==='book2'){
+    image = image + '<img src="book2.jpg">'
+  }
+}
+  document.write(image)
+
+}
+
+gettingBooks()
